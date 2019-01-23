8e64fd6363b14a0e8ade64977e0ce64d9b4f0339
Dinngger <41265073+Dinngger@users.noreply.github.com>
2019-01-23 19:12:19 +0800

    The first version.
    Finish the basic Q-learning program. It's simple but does work.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Q_learning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "石头";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "剪刀";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(352, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "布";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "机器：0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "你：0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(147, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 34);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "机器：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(112, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(259, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "五局三胜制 总比分： 0:0:0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "平：0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 340);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "强化学习小游戏 —— Dinger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q_learning
+{
+    public partial class Form1 : Form
+    {
+        Bot mybot;
+        public Form1()
+        {
+            mybot = new Bot();
+            InitializeComponent();
+        }
+
+        private void LabelPaint()
+        {
+            switch(mybot.Last)
+            {
+                case 0: label3.Text = "机器：石头";break;
+                case 1: label3.Text = "机器：剪刀";break;
+                case 2: label3.Text = "机器： 布 ";break;
+                default:label3.Text = "机器：";break;
+            }
+            label4.Text = string.Format("五局三胜制 总比分： {0}:{1}:{2}", mybot.Win, mybot.Even, mybot.Lose);
+            label1.Text = string.Format("机器：{0}", mybot.Thiswin);
+            label5.Text = string.Format("平：{0}", mybot.Thiseven);
+            label2.Text = string.Format("你：{0}", mybot.Thislose);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mybot.NextS(0);
+            LabelPaint();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mybot.NextS(1);
+            LabelPaint();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mybot.NextS(2);
+            LabelPaint();
+        }
+    }
+
+    public class Bot
+    {
+        int win;
+        int lose;
+        int even;
+        int thiswin;
+        int thiseven;
+        int thislose;
+        int[] lasthree = new int[3] { 5, 5, 5 };
+        int counter = 0;
+        double[] Q0 = new double[3];
+        double[,] Q1 = new double[3, 3];
+        double[,,] Q2 = new double[3, 3, 3];
+        double[,,,] Q3 = new double[3, 3, 3, 3];
+        double[,,,,] Q4 = new double[3, 3, 3, 3, 3];
+        int step;
+        int[] state = new int[5];
+        int laststep = -1;
+        int thisstep;
+        Random rd = new Random();
+
+        public int Win => win;
+        public int Lose => lose;
+        public int Even => even;
+        public int Thiswin => thiswin;
+        public int Thiseven => thiseven;
+        public int Thislose => thislose;
+        public int Last => laststep;
+
+        private int Is_win(int a, int b)
+        {
+            if (a == b)
+                return 2;
+            else if (a == b - 1 || (a == 2 && b == 0))
+                return 3;
+            else
+                return 1;
+        }
+
+        private void Init()
+        {
+            step = 0;
+            thiseven = 0;
+            thislose = 0;
+            thiswin = 0;
+        }
+
+        private void Count(int w)
+        {
+            lasthree[counter] = w;
+            counter++;
+            if (counter > 2)
+                counter = 0;
+        }
+
+        private void ReQ(ref double L, double N, double rate)
+        {
+            if (L == 0)
+                L = N;
+            else
+                L = (L + rate * N) / (1 + rate);
+        }
+
+        public void NextS(int input)
+        {
+            state[step] = input;
+            laststep = thisstep;
+            int iswin = Is_win(laststep, input);
+            if (iswin == 1)
+                thislose++;
+            else if (iswin == 3)
+                thiswin++;
+            else
+                thiseven++;
+            double rate = (lasthree[0] + lasthree[1] + lasthree[2]) / 15.0;
+            switch (step)
+            {
+                case 0: ReQ(ref Q0[laststep], iswin, rate); break;
+                case 1: ReQ(ref Q1[state[0], laststep], iswin, rate); break;
+                case 2: ReQ(ref Q2[state[0], state[1], laststep], iswin, rate); break;
+                case 3: ReQ(ref Q3[state[0], state[1], state[2], laststep], iswin, rate); break;
+                case 4: ReQ(ref Q4[state[0], state[1], state[2], state[3], laststep], iswin, rate);break;
+                default:break;
+            }
+
+            step++;
+            if (step > 4)
+                step = 0;
+
+            if (thiswin > 2 || (step == 0 && thiswin > thislose))
+            {
+                MessageBox.Show("你输了");
+                win++;
+                Init();
+                Count(5);
+            }
+            else if (thislose > 2 || (step == 0 && thiswin < thislose))
+            {
+                MessageBox.Show("你赢了");
+                lose++;
+                Init();
+                Count(3);
+            }
+            else if (step == 0 && thiswin == thislose)
+            {
+                MessageBox.Show("平局");
+                even++;
+                Init();
+                Count(4);
+            }
+            double[] theQ = new double[3];
+            for (int i=0; i<3; i++)
+            {
+                double thisQ;
+                switch (step)
+                {
+                    case 0: thisQ = Q0[i]; break;
+                    case 1: thisQ = Q1[state[0], i]; break;
+                    case 2: thisQ = Q2[state[0], state[1], i]; break;
+                    case 3: thisQ = Q3[state[0], state[1], state[2], i]; break;
+                    case 4: thisQ = Q4[state[0], state[1], state[2], state[3], i]; break;
+                    default: thisQ = 0; break;
+                }
+                theQ[i] = thisQ;
+            }
+            int explore = 0;
+            for (int i=0; i<3; i++)
+            {
+                if (theQ[i] == 0)
+                    explore++;
+            }
+            if (explore == 3)
+                thisstep = rd.Next(0, 3);
+            else if (explore == 2)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (theQ[i] != 0)
+                    {
+                        if (rd.Next(0, 2) == 0)
+                        {
+                            thisstep = i;
+                        }
+                        else
+                        {
+                            thisstep = rd.Next(0, 3);
+                            while (thisstep == i)
+                            {
+                                thisstep = rd.Next(0, 3);
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            else if (explore == 1)
+            {
+                for (int i=0; i<3; i++)
+                {
+                    if (theQ[i] == 0)
+                    {
+                        if (rd.Next(0, 3) == 0)
+                            thisstep = i;
+                        else
+                        {
+                            for (int j=0; j<3; j++)
+                            {
+                                if (theQ[j] == theQ.Max())
+                                    thisstep = j;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i=0; i<3; i++)
+                {
+                    if (theQ[i] == theQ.Max())
+                        thisstep = i;
+                }
+            }
+        }
+        public Bot()
+        {
+            Q0.Initialize();
+            Q1.Initialize();
+            Q2.Initialize();
+            Q3.Initialize();
+            Q4.Initialize();
+            thisstep = rd.Next(0, 3);
+            Init();
+            win = 0;
+            lose = 0;
+            even = 0;
+        }
+    }
+}
